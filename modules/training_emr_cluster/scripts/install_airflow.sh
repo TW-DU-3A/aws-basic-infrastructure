a0b0722526d302975a186e5bf1637c5b6f4c4b6e
Vivek Soni <erviveksoni@gmail.com>
2020-03-20 14:46:06 +0530

    Add the shell scripts to install airflow on emr-master

---
+++ modules/training_emr_cluster/scripts/install_airflow.sh
@@ -0,0 +1,5 @@
+sudo yum update
+sudo yum install yum-utils
+sudo yum groupinstall development
+sudo yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python-devel wget cyrus-sasl-devel.x86_64
+sudo yum -y install python-pip
