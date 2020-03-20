6b5d05168511bdc07a195d7274988f5b4703d154
Swathi Varkala <swathiv@ip-192-168-0-7.ap-southeast-1.compute.internal>
2020-03-20 16:55:16 +0530

    Digi,Swathi | Install Python3 and make it dafault to airflow

--- modules/training_airflow_2/scripts/install_airflow.sh
+++ modules/training_airflow_2/scripts/install_airflow.sh
@@ -1,8 +1,10 @@
 set -x
 yum -y groupinstall "Development Tools"
 yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python-devel wget cyrus-sasl-devel.x86_64
-yum -y install python-pip
-pip install apache-airflow[s3]
+sudo yum install python37
+alias python=python3
+yum -y install python3-pip
+pip3 install apache-airflow[s3]
 airflow initdb
 airflow scheduler -D
 airflow webserver -D
