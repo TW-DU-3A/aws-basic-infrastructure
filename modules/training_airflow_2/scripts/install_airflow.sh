eb2e39466cd2f09a6e76bf4d706356e4d34fa2d6
Digvijay Gunjal <digvijag@thoughtworks.com>
2020-03-23 14:19:26 +0530

    Swathi, Digi | Use virtuaenv for python3 and airflow.

--- modules/training_airflow_2/scripts/install_airflow.sh
+++ modules/training_airflow_2/scripts/install_airflow.sh
@@ -1,10 +1,15 @@
+#Install virtualenv and python3
+sudo yum install python37 python-pip
+pip install -U pip
+pip install -U virtualenv
+virtualenv -p python3 airflow-python-3
+cd airflow-python-3 && source bin/activate
+
 set -x
 yum -y groupinstall "Development Tools"
 yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python-devel wget cyrus-sasl-devel.x86_64
-sudo yum install python37
-alias python=python3
-yum -y install python3-pip
-pip3 install apache-airflow[s3]
+
+pip install apache-airflow[s3]
 airflow initdb
 airflow scheduler -D
 airflow webserver -D
