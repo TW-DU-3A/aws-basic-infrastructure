558f1b73341955ead0b4c8878c8138dc15b42c0b
Digvijay Gunjal <digvijag@thoughtworks.com>
2020-03-23 15:37:12 +0530

    Swathi, Digi | install dependencies with correct versions

--- modules/training_airflow_2/scripts/install_airflow.sh
+++ modules/training_airflow_2/scripts/install_airflow.sh
@@ -2,16 +2,14 @@
 sudo yum install python37 python3-pip
 pip3 install -U virtualenv
 
-virtualenv -p python3 airflow-python-3
+python3 -m virtualenv airflow-python-3
 cd airflow-python-3 && source bin/activate
 
 set -x
 yum -y groupinstall "Development Tools"
-yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python-devel wget cyrus-sasl-devel.x86_64
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python3-devel wget cyrus-sasl-devel.x86_64 python3-setuptools gcc-c++
 
-python --version
-
-pip3 install apache-airflow[s3]
+pip3 install apache-airflow
 airflow initdb
 airflow scheduler -D
 airflow webserver -D
