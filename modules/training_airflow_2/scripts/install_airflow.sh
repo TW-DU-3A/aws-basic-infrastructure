4c1cc2f545fd051ebf8c2f913666cff47e048a88
Swathi Varkala <swathiv@Swathis-MacBook-Pro.local>
2020-03-24 15:33:36 +0530

    Digi,Swathi | Fix Yum install with -y and Setup Livy Operator

--- modules/training_airflow_2/scripts/install_airflow.sh
+++ modules/training_airflow_2/scripts/install_airflow.sh
@@ -1,5 +1,5 @@
 #Install virtualenv and python3
-sudo yum install python37 python3-pip
+sudo yum -y install python37 python3-pip
 pip3 install -U virtualenv
 
 python3 -m virtualenv airflow-python-3
@@ -7,9 +7,21 @@ cd airflow-python-3 && source bin/activate
 
 set -x
 yum -y groupinstall "Development Tools"
-yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python3-devel wget cyrus-sasl-devel.x86_64 python3-setuptools gcc-c++
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel python3-devel wget cyrus-sasl-devel.x86_64 python3-setuptools gcc-c++
 
 pip3 install apache-airflow
+
+mkdir -p /root/airflow/plugins
+
+wget -O /root/airflow/plugins/livy_hook.py https://raw.githubusercontent.com/JituS/spark-etl/master/airflow/plugins/livy_hook.py
+wget -O /root/airflow/plugins/livy_sensor.py https://raw.githubusercontent.com/JituS/spark-etl/master/airflow/plugins/livy_sensor.py
+wget -O /root/airflow/plugins/livy_operator.py https://raw.githubusercontent.com/JituS/spark-etl/master/airflow/plugins/livy_operator.py
+
+export INPUT_PATH=hdfs:///user/hadoop/swathiv/input
+export RAW_PATH=hdfs:///user/hadoop/swathiv/raw
+export DESTINATION_PATH=hdfs:///user/hadoop/swathiv/destination
+export ERROR_PATH=hdfs:///user/hadoop/swathiv/error
+
 airflow initdb
 airflow scheduler -D
 airflow webserver -D
